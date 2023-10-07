a77f468d4d11d57e49c190c241876cf75f3686df
tienifr <christianwen18@gmail.com>
2023-10-07 11:46:40 +0700

    fix: 29030

--- src/components/AddressSearch/index.js
+++ src/components/AddressSearch/index.js
@@ -294,12 +294,12 @@ function AddressSearch(props) {
                         )
                     }
                     listLoaderComponent={
-                        <View style={[styles.pv4]}>
+                        props.network.isOffline ? null : (<View style={[styles.pv4]}>
                             <ActivityIndicator
                                 color={themeColors.spinner}
                                 size="small"
                             />
-                        </View>
+                        </View>)
                     }
                     renderHeaderComponent={() =>
                         !props.value &&
